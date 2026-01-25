db19323f5f2476e421fb7a5348d0309ec6a8935a
Perilix <166854906+Perilix@users.noreply.github.com>
2026-01-25 13:02:40 +0100

    hotfix: edit env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://trainwise-backend-rnd4.onrender.com/api',
+  apiUrl: 'https://trainwise-backend-rnd4.onrender.com',
   socketUrl: 'https://trainwise-backend-rnd4.onrender.com'
 };
